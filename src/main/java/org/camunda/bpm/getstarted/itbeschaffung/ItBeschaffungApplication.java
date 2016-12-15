4d4d29db46b5c73196dd533833c1981fd894460d
Wenxin Hu <hu@th-brandenburg.de>
2016-12-15 13:51:25 +0100

    DMN läuft

--- src/main/java/org/camunda/bpm/getstarted/itbeschaffung/ItBeschaffungApplication.java
+++ src/main/java/org/camunda/bpm/getstarted/itbeschaffung/ItBeschaffungApplication.java
@@ -3,9 +3,10 @@ package org.camunda.bpm.getstarted.itbeschaffung;
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
 
+
 @ProcessApplication("IT Beschaffung App")
 public class ItBeschaffungApplication extends ServletProcessApplication {
 
-	//empty implementation
-
+//empty implementation
+	
 }
